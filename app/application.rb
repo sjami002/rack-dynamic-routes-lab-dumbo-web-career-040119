035ae3f0bb2816f99b8d17ffcb584427afce9845
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 20:51:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
  
  if req.path.match(/items/)
    
-    item_name = req.path.split("/item/").last
+    item_name = req.path.split("/items/").last
     
     if item = @@items.find do |i| 
       i.name == item_name
