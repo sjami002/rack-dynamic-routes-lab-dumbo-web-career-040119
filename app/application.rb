07307d0947f96500d3505084a6ba9db70e8f9bbe
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-22 20:46:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
    
     item_name = req.path.split("/item/").last
     
-    if item = @@items.find{|i| item.name == item_name}
+    if item = @@items.find do |i| item.name == item_name}
     
     end
  
